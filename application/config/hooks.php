9ecbf01a1da39bf793a9cec7655c3e0eafb0eef9
Agung Dirgantara <agungmasda29@gmail.com>
2021-10-17 03:57:39 +0700

    update hook config

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,73 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+/**
+ * -------------------------------------------------------------------------
+ * Pre System
+ * -------------------------------------------------------------------------
+ *
+ * Called very early during system execution.
+ * Only the benchmark and hooks class have been loaded at this point.
+ * No routing or other processes have happened.
+ */
+$hook['pre_system'] = NULL;
+
+/**
+ * -------------------------------------------------------------------------
+ * Pre Controller
+ * -------------------------------------------------------------------------
+ *
+ * Called immediately prior to any of your controllers being called.
+ * All base classes, routing, and security checks have been done.
+ */
+$hook['pre_controller'] = NULL;
+
+/**
+ * -------------------------------------------------------------------------
+ * Post Controller Constructor
+ * -------------------------------------------------------------------------
+ *
+ * Called immediately after your controller is instantiated, but prior to any method calls happening.
+ */
+$hook['post_controller_constructor'] = NULL;
+
+/**
+ * -------------------------------------------------------------------------
+ * Post Controller
+ * -------------------------------------------------------------------------
+ *
+ * Called immediately after your controller is fully executed.
+ */
+$hook['post_controller'] = NULL;
+
+/**
+ * -------------------------------------------------------------------------
+ * Display Override
+ * -------------------------------------------------------------------------
+ *
+ * Overrides the _display() method, used to send the finalized page to the web browser at the end of system execution.
+ * This permits you to use your own display methodology.
+ * Note that you will need to reference the CI superobject with $this->CI =& get_instance() and then the finalized data will be available by calling
+ * $this->CI->output->get_output()
+ */
+$hook['display_override'] = NULL;
+
+/**
+ * -------------------------------------------------------------------------
+ * Cache Override
+ * -------------------------------------------------------------------------
+ *
+ * Enables you to call your own method instead of the _display_cache() method in the Output Library.
+ * This permits you to use your own cache display mechanism.
+ */
+$hook['cache_override'] = NULL;
+
+/**
+ * -------------------------------------------------------------------------
+ * Post System
+ * -------------------------------------------------------------------------
+ *
+ * Called after the final rendered page is sent to the browser, at the end of system execution after the finalized data is sent to the browser.
+ */
+$hook['post_system'] = NULL;
