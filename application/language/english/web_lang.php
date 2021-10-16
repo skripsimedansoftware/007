75cb14d34f5ad75b71ba3e5b17fc8765ab839b0a
Agung Dirgantara <agungmasda29@gmail.com>
2021-10-17 04:33:00 +0700

    added web language

---
+++ application/language/english/web_lang.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package Codeigniter
+ * @subpackage Web
+ * @category Language
+ * @author Agung Dirgantara <agungmasda29@gmail.com>
+ */
+
+
+
+/* End of file web_lang.php */
+/* Location : ./application/language/english/web_lang.php */
