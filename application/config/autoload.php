493889224a0868d5d50e14cf5badb3808903bb22
Agung Dirgantara <agungmasda29@gmail.com>
2021-11-09 05:55:53 +0700

    autoload required models

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -133,4 +133,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('data_training_name', 'data_training_data');
