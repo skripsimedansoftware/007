b433e3e3d4f5ba7fd1ef2831354f80bc7c443646
Agung Dirgantara <agungmasda29@gmail.com>
2021-11-24 03:47:27 +0700

    update home page

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -7,7 +7,7 @@
 <section class="content container-fluid">
 	<!-- Small boxes (Stat box) -->
 	<div class="row">
-		<div class="col-lg-4 col-xs-12">
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 			<!-- small box -->
 			<div class="small-box bg-aqua">
 				<div class="inner">
@@ -21,7 +21,7 @@
 			</div>
 		</div>
 		<!-- ./col -->
-		<div class="col-lg-4 col-xs-12">
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 			<!-- small box -->
 			<div class="small-box bg-green">
 				<div class="inner">
@@ -35,7 +35,7 @@
 			</div>
 		</div>
 		<!-- ./col -->
-		<div class="col-lg-4 col-xs-12">
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 			<!-- small box -->
 			<div class="small-box bg-yellow">
 				<div class="inner">
@@ -47,70 +47,6 @@
 				</div>
 				<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
 			</div>
-		</div>
-	</div>
-	<!-- /.row -->
-	<div class="row">
-		<div class="col-md-3 col-sm-6 col-xs-12">
-			<div class="info-box">
-				<span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
-				<div class="info-box-content">
-					<span class="info-box-text">CPU Traffic</span>
-					<span class="info-box-number">90<small>%</small></span>
-				</div>
-				<!-- /.info-box-content -->
-			</div>
-			<!-- /.info-box -->
-		</div>
-		<!-- /.col -->
-		<div class="col-md-3 col-sm-6 col-xs-12">
-			<div class="info-box">
-				<span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
-				<div class="info-box-content">
-					<span class="info-box-text">Likes</span>
-					<span class="info-box-number">41,410</span>
-				</div>
-				<!-- /.info-box-content -->
-			</div>
-			<!-- /.info-box -->
-		</div>
-		<!-- /.col -->
-		<!-- fix for small devices only -->
-		<div class="clearfix visible-sm-block"></div>
-		<div class="col-md-3 col-sm-6 col-xs-12">
-			<div class="info-box">
-				<span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
-				<div class="info-box-content">
-					<span class="info-box-text">Sales</span>
-					<span class="info-box-number">760</span>
-				</div>
-				<!-- /.info-box-content -->
-			</div>
-			<!-- /.info-box -->
-		</div>
-		<!-- /.col -->
-		<div class="col-md-3 col-sm-6 col-xs-12">
-			<div class="info-box">
-				<span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
-				<div class="info-box-content">
-					<span class="info-box-text">New Members</span>
-					<span class="info-box-number">2,000</span>
-				</div>
-				<!-- /.info-box-content -->
-			</div>
-			<!-- /.info-box -->
-		</div>
-		<!-- /.col -->
-	</div>
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">Title</h3>
-		</div>
-		<div class="box-body">
-			Start creating your amazing application!
-		</div>
-		<div class="box-footer">
-			Footer
 		</div>
 	</div>
 </section>
