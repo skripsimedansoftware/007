5b028bcc75627dcf3dec5597267ce42c9507cb7a
agoenks29D <agungmasda29@gmail.com>
2021-12-16 13:48:44 +0700

    changed script

--- server/public/script.js
+++ server/public/script.js
@@ -1,49 +1,43 @@
+window.knnClassifier = ml5.KNNClassifier();
+window.featureExtractor = ml5.featureExtractor('MobileNet', readyToUse);
+window.serverURL = 'https://cek-kematangan-alpukat.uinsu.my.id';
+
+function readyToUse() {
+	$.ajax({
+		url: window.serverURL+'/admin/try',
+		type: 'GET',
+		dataType: 'JSON',
+		crossDomain: true,
+		success: function(data) {
+			var total_data = data.length;
+			$.each(data, function(index, val) {
+				// temp image to get image widh & height
+				var temp_image = new Image();
+				temp_image.onload = function() {
+					// temp image loaded
+					var img = new Image(this.width, this.height);
+					img.onload = function() {
+						img.crossOrigin = 'Anonymous';
+						window.knnClassifier.addExample(window.featureExtractor.infer(img), val.title);
+					}
+
+					img.src = 'https://alpukat-files.uinsu.my.id/'+val.image;
+				}
+
+				temp_image.src = 'https://alpukat-files.uinsu.my.id/'+val.image;
+			});
+		},
+		error: function(error) {
+
+		}
+	});
+}
+
 var socket = io();
 socket.on('connect', function() {
 
 	// join as checker
 	socket.emit('join_check');
-
-	window.knnClassifier = ml5.KNNClassifier();
-	window.featureExtractor = ml5.featureExtractor('MobileNet', readyToUse);
-	window.serverURL = 'https://cek-kematangan-alpukat.uinsu.my.id';
-
-	function readyToUse() {
-		$.ajax({
-			url: window.serverURL+'/admin/try',
-			type: 'GET',
-			dataType: 'JSON',
-			crossDomain: true,
-			success: function(data) {
-				var total_data = data.length;
-				$.each(data, function(index, val) {
-					// temp image to get image widh & height
-					var temp_image = new Image();
-					temp_image.onload = function() {
-						// temp image loaded
-						var img = new Image(this.width, this.height);
-						img.onload = function() {
-							if ((index+1) == total_data) {
-								socket.emit('loaded', {
-									data: (index+1),
-									total: total_data
-								});
-							}
-							img.crossOrigin = 'Anonymous';
-							window.knnClassifier.addExample(window.featureExtractor.infer(img), val.title);
-						}
-
-						img.src = 'https://alpukat-files.uinsu.my.id/'+val.image;
-					}
-
-					temp_image.src = 'https://alpukat-files.uinsu.my.id/'+val.image;
-				});
-			},
-			error: function(error) {
-
-			}
-		});
-	}
 
 	socket.on('check', function(data) {
 		var temp_image = new Image();
@@ -54,10 +48,7 @@ socket.on('connect', function() {
 				const features = window.featureExtractor.infer(img);
 				// Use KNN Classifier to classify these features
 				window.knnClassifier.classify(features, (error, result) => {
-					console.log(result)
-					if (result !== null || result !== undefined) {
-						socket.emit('checked', {socket_id: data.socket_id, result: result, colors: data.colors});
-					}
+					socket.emit('checked', {socket_id: data.socket_id, result: result, colors: data.colors});
 				});
 			}
 
