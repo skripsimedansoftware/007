596c41c870ba9b3c7196889547d3039c2c76d9fb
agoenks29D <agungmasda29@gmail.com>
2021-12-16 11:35:48 +0700

    forget fucking get

--- server/app.js
+++ server/app.js
@@ -109,7 +109,7 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('checked', function(data) {
-		var KNN_Prediction = KNNClassifier.predict(data)[0].split('-');
+		var KNN_Prediction = KNNClassifier.predict(data.colors)[0].split('-');
 		var find_label = find_value(data_count, 'id', KNN_Prediction[1]);
 		var total_data = data_count[find_label].images_count*data_count[find_label].colors_count;
 		var data_found = (parseInt(KNN_Prediction[2])*parseInt(KNN_Prediction[3]));
