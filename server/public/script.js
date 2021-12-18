8fade06ef1507f7f82adac243d42c8403c33f17c
agoenks29D <agungmasda29@gmail.com>
2021-12-18 08:24:14 +0700

    one load fuck

--- server/public/script.js
+++ server/public/script.js
@@ -6,24 +6,17 @@ var socket = io({ transports: ['websocket', 'polling'] });
 socket.on('connect', function() {
 	// join as checker
 	socket.emit('join_check');
-
 	socket.on('check', function(data) {
-		var temp_image = new Image();
-		temp_image.onload = function() {
-			// temp image loaded
-			var img = new Image(this.width, this.height);
-			img.onload = function() {
-				const features = window.featureExtractor.infer(img);
-				// Use KNN Classifier to classify these features
-				window.knnClassifier.classify(features, (error, result) => {
-					socket.emit('checked', {socket_id: data.socket_id, result: result, colors: data.colors});
-				});
-			}
-
-			img.src = data.image;
+		var img = new Image();
+		img.onload = function() {
+			const features = window.featureExtractor.infer(img);
+			// Use KNN Classifier to classify these features
+			window.knnClassifier.classify(features, (error, result) => {
+				socket.emit('checked', {socket_id: data.socket_id, result: result, colors: data.colors});
+			});
 		}
 
-		temp_image.src = data.image;
+		img.src = data.image;
 	});
 });
 
